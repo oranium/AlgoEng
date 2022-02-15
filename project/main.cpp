1baa7c290285655c00e7ced6d25ab93799b415c0
oranium <aron.gaden@gmail.com>
2022-02-15 14:40:52 +0100

    🎨 Simplify main

--- project/main.cpp
+++ project/main.cpp
@@ -28,48 +28,17 @@ int main(int argc, char *argv[]) {
     }
     std::cout << "Writing to " << output << std::endl;
 
-    std::vector<double> filter{1.0/3, 1.0/3, 1.0/3};
 
     ppm img(path);
 
-    /*
-    std::vector<double> convRT;
-    convRT.resize(img.size);
-    transpose(img.r, convRT, img.height, img.width);
-    convRT = convolve1D(filter, convRT, img.width, img.height);
-    std::vector<double> convR;
-    convR.resize(img.size);
-    transpose(convRT, convR, img.width, img.height);
-    convR = convolve1D(filter, convR, img.height, img.width);
+    std::vector<double> r = gaussFilter(img.r, 5, 30, img.height, img.width);
+    std::vector<double> g = gaussFilter(img.g,5,30, img.height, img.width);
+    std::vector<double> b = gaussFilter(img.b, 5,30, img.height, img.width);
 
-    std::vector<double> convGT;
-    convGT.resize(img.size);
-    transpose(img.g, convGT, img.height, img.width);
-    convGT = convolve1D(filter, convGT, img.width, img.height);
-    std::vector<double> convG;
-    convG.resize(img.size);
-    transpose(convGT, convG, img.width, img.height);
-    convG = convolve1D(filter, convG, img.height, img.width);
-
-    std::vector<double> convBT;
-    convBT.resize(img.size);
-    transpose(img.b, convBT, img.height, img.width);
-    convBT = convolve1D(filter, convBT, img.width, img.height);
-    std::vector<double> convB;
-    convB.resize(img.size);
-    transpose(convBT, convB, img.width, img.height);
-    convB = convolve1D(filter, convB, img.height, img.width);
-    */
-
-    std::vector<double> r_blur = gaussFilter(img.r, 5, 30, img.height, img.width);
-    std::vector<double> g_blur = gaussFilter(img.g,5,30, img.height, img.width);
-    std::vector<double> b_blur = gaussFilter(img.b, 5,30, img.height, img.width);
-
-    std::vector<double> r_sigma = sigmaFilter(img.r, r_blur, 100000);
-    std::vector<double> g_sigma = sigmaFilter(img.g, g_blur, 100000);
-    std::vector<double> b_sigma = sigmaFilter(img.b, b_blur, 100000);
-
-    ppm imgCvd(r_sigma, g_sigma, b_sigma, img.height, img.width);
+    r = sigmaFilter(img.r, r, 65000);
+    g = sigmaFilter(img.g, g, 65000);
+    b = sigmaFilter(img.b, b, 65000);
+    ppm imgCvd(r, g, b, img.height, img.width);
 
     imgCvd.normalize();
     imgCvd.write(output);
