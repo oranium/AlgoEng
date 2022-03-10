7e2b44af6e2f7bb1b0470e1b6420e59a4c6d481b
BrnRnhld <33638121+BrnRnhld@users.noreply.github.com>
2022-03-10 21:17:12 +0100

    filter tests

--- project/tests.cpp
+++ project/tests.cpp
@@ -3,6 +3,7 @@
 //
 #include "aligned_allocator.h"
 #include "Matrix2D.h"
+#include "filter.h"
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE( "Transpose is correct", "[transpose]" ) {
@@ -16,3 +17,49 @@ transpose(in, out, N, M);
 //Check element-wise equality
 REQUIRE(out == expected );
 }
+
+TEST_CASE("Gaussian blur test"){
+
+    aligned_vector<double> img = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    aligned_vector<double> blurred_img = {255, 255, 255, 255, 255, 0, 254, 33, 0, 10, 255, 255};
+    aligned_vector<double> expected = {255, 255, 255, 255, 255, 6, 7, 8, 9, 10, 255, 255};
+
+    aligned_vector<double> out(img.size());
+
+    out = gaussFilter(img, blurred_img);
+    REQUIRE(out == expected);
+}
+
+TEST_CASE("custom contrast filter test"){
+
+    aligned_vector<double> img = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    aligned_vector<double> blurred_img = {255, 255, 255, 255, 255, 0, 254, 33, 0, 10, 255, 255};
+    aligned_vector<double> expected = {255, 255, 255, 255, 255, 6, 7, 8, 9, 10, 255, 255};
+
+    aligned_vector<double> out(img.size());
+
+    out = sigmaFilter(img, blurred_img);
+    REQUIRE(out == expected);
+}
+
+TEST_CASE("thresholding works correctly"){
+    aligned_vector<double> in = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    aligned_vector<double> out(in.size());
+    aligned_vector<double> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 255, 255};
+
+    out = thresholding(in, 10);
+    REQUIRE(out == expected);
+}
+
+TEST_CASE("removeBackground test "){
+
+    aligned_vector<double> in = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    aligned_vector<double> mask = {255, 255, 255, 255, 255, 0, 254, 33, 0, 10, 255, 255};
+    aligned_vector<double> expected = {255, 255, 255, 255, 255, 6, 7, 8, 9, 10, 255, 255};
+
+    aligned_vector<double> out(in.size());
+
+    out = removeBackground(in, mask);
+    REQUIRE(out == expected);
+}
+
